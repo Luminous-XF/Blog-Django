54d51d6dc3c34e7b3bd3c096b0de965a033feea5
Luminous <1094038421@qq.com>
2023-11-22 23:58:12 +0800

    新建建表操作

--- blogapp/models.py
+++ blogapp/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class UserInfo(models.Model):
     name = models.CharField(max_length=32)
     age = models.IntegerField()
+    password = models.CharField(max_length=32, null=True, blank=True)
