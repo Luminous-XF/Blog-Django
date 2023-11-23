f6aaa571e00e270e96b904ac08467c082cd8b221
Luminous <1094038421@qq.com>
2023-11-23 23:58:29 +0800

    新建插入数据操作

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse
+from blogapp.models import UserInfo
 
 
 def index(request):
+    UserInfo.objects.create(name="蹦蹦", age="12", password="abc")
     return HttpResponse("Hello World!")
