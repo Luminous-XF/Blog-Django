4c1476bcb03a395a3cbdb8ab8e14de3edc887915
xiang <1582878048@qq.com>
2023-12-05 00:42:08 +0800

    post类定义

--- blogapp/models.py
+++ blogapp/models.py
@@ -8,4 +8,30 @@ class UserInfo(models.Model):
 
 
 class Post(models.Model):
-    pass
+    uuid = models.CharField(max_length=36)
+    user_id = models.CharField(max_length=36)
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    STATUS_CHOICE = [
+        ('COMMON', 'common'),
+        ('TOP', 'top'),
+        ('ESSENCE', 'essence'),
+    ]
+    type = models.CharField(max_length=10, choices=STATUS_CHOICE,default='COMMON')
+    comment_count = models.BigIntegerField(default=0)
+    score = models.FloatField(default=0.0)
+    STATUS_CHOICE = [
+        ('NORMAL', 'normal'),
+        ('AUDIT', 'audit'),
+        ('BANNED', 'banned'),
+    ]
+    status = models.CharField(max_length=10 , choices=STATUS_CHOICE,default='AUDIT')
+    likes = models.BigIntegerField(default=0)
+    page_views = models.BigIntegerField(default=0)
+    create_time = models.DateTimeField()
+    update_time = models.DateTimeField()
+    STATUS_CHOICE = [
+        ('NORMAL', 'normal'),
+        ('DELETE', 'delete'),
+    ]
+    row_status = models.CharField(max_length=10,choices=STATUS_CHOICE,default='NORMAL')
