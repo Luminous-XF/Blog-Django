a7f493cc5e2e1b02009b4ee10854645006d7b783
xiang <1582878048@qq.com>
2023-12-04 23:31:12 +0800

    添加Post实体类

--- blogapp/models.py
+++ blogapp/models.py
@@ -5,3 +5,7 @@ class UserInfo(models.Model):
     name = models.CharField(max_length=32)
     age = models.IntegerField()
     password = models.CharField(max_length=32, null=True, blank=True)
+
+
+class Post(models.Model):
+    pass
