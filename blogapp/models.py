76309932c8cf226ed1ef7e91fe40b1372e53826b
xiang <1582878048@qq.com>
2023-12-06 00:26:51 +0800

    类定义枚举

--- blogapp/models.py
+++ blogapp/models.py
@@ -6,35 +6,60 @@ class UserInfo(models.Model):
     age = models.IntegerField()
     password = models.CharField(max_length=32, null=True, blank=True)
 
+class PostTypeChoice(models.TextChoices):
+    COMMON = 'COMMON', 'Common'
+    TOP = 'TOP', 'Top'
+    ESSENCE = 'ESSENCE', 'Essence'
+
+class PoststatusChoice(models.TextChoices):
+    NORMAL = 'NORMAL', 'Normal'
+    AUDIT = 'AUDIT', 'Audit'
+    BANNED = 'BANNED', 'Banned'
+
+class PostRow_StatusChoice(models.TextChoices):
+    NORMAL = 'NORMAL', 'Normal'
+    DELETE = 'DELETE', 'Delete'
 
 class Post(models.Model):
     uuid = models.CharField(max_length=36)
     user_id = models.CharField(max_length=36)
     title = models.CharField(max_length=256)
     content = models.TextField()
-    STATUS_CHOICE = [
-        ('COMMON', 'common'),
-        ('TOP', 'top'),
-        ('ESSENCE', 'essence'),
-    ]
-    type = models.CharField(max_length=10, choices=STATUS_CHOICE, default='COMMON')
+    type = models.CharField(
+        max_length=64,
+        choices=PostTypeChoice.choices,
+        default=PostTypeChoice.COMMON)
     comment_count = models.BigIntegerField(default=0)
     score = models.FloatField(default=0.0)
-    STATUS_CHOICE = [
-        ('NORMAL', 'normal'),
-        ('AUDIT', 'audit'),
-        ('BANNED', 'banned'),
-    ]
-    status = models.CharField(max_length=10,  choices=STATUS_CHOICE, default='AUDIT')
+    status = models.CharField(
+        max_length=64,
+        choices=PoststatusChoice.choices,
+        default=PoststatusChoice.AUDIT
+    )
     likes = models.BigIntegerField(default=0)
     page_views = models.BigIntegerField(default=0)
     create_time = models.DateTimeField()
     update_time = models.DateTimeField()
-    STATUS_CHOICE = [
-        ('NORMAL', 'normal'),
-        ('DELETE', 'delete'),
-    ]
-    row_status = models.CharField(max_length=10, choices=STATUS_CHOICE, default='NORMAL')
+    row_status = models.CharField(
+        max_length=64,
+        choices=PostRow_StatusChoice.choices,
+        default=PostRow_StatusChoice.NORMAL
+    )
+
+
+class UserUser_TypeChoice(models.TextChoices):
+    ADMIN = 'ADMIN', 'Admin'
+    COMMON = 'COMMON', 'Common'
+
+class UserUser_StatusChoice(models.TextChoices):
+    COMMON = 'COMMON', 'Common'
+    BANNED = 'BANNED', 'Banned'
+    INACTIVATION = 'INACTIVATION', 'Inactivation'
+
+class UserGenderChoice(models.TextChoices):
+    MALE ='MALE', 'Male'
+    FEMALE ='FEMALE', 'Female'
+    UNKNOWN = 'UNKNOWN', 'Unknown'
 
 class User(models.Model):
     uuid = models.CharField(max_length=36)
@@ -43,26 +68,21 @@ class User(models.Model):
     password = models.CharField(max_length=32)
     salt = models.CharField(max_length=16)
     email = models.CharField(max_length=32)
-    STATUS_CHOICE = [
-        ('ADMIN', 'Admin'),
-        ('COMMON', 'Common'),
-    ]
-    user_type = models.CharField(max_length=10, choices=STATUS_CHOICE, default='COMMON')
-    STATUS_CHOICE = [
-        ('COMMON', 'Common'),
-        ('BANNED', 'Banned'),
-        ('INACTIVATION', 'Inactivation'),
-    ]
-    user_satus = models.CharField(max_length=10, choices=STATUS_CHOICE, default='INACTIVATION')
+    user_type = models.CharField(
+        max_length=64,
+        choices=UserUser_TypeChoice.choices,
+        default=UserUser_TypeChoice.COMMON)
+    user_satus = models.CharField(
+        max_length=64,
+        choices=UserUser_StatusChoice.choices,
+        default=UserUser_StatusChoice.INACTIVATION)
     activation_code = models.CharField(max_length=32)
     header_url = models.CharField(max_length=256)
     create_time = models.DateTimeField()
-    STATUS_CHOICE = [
-        ('MALE', 'Male'),
-        ('FEMALE', 'Female'),
-        ('UNKNOWN', 'Unknown')
-    ]
-    gender = models.CharField(max_length=10, choices=STATUS_CHOICE, default='UNKNOWN')
+    gender = models.CharField(
+        max_length=64,
+        choices=UserGenderChoice.choices,
+                default=UserGenderChoice.UNKNOWN)
     brief = models.TextField()
     likes = models.BigIntegerField(default=0)
     page_views = models.BigIntegerField(default=0)
