59bca9c480ed8327cb04921cbf06074abe6b8dc0
xiang <1582878048@qq.com>
2023-12-05 22:40:03 +0800

    user类定义

--- blogapp/models.py
+++ blogapp/models.py
@@ -17,7 +17,7 @@ class Post(models.Model):
         ('TOP', 'top'),
         ('ESSENCE', 'essence'),
     ]
-    type = models.CharField(max_length=10, choices=STATUS_CHOICE,default='COMMON')
+    type = models.CharField(max_length=10, choices=STATUS_CHOICE, default='COMMON')
     comment_count = models.BigIntegerField(default=0)
     score = models.FloatField(default=0.0)
     STATUS_CHOICE = [
@@ -25,7 +25,7 @@ class Post(models.Model):
         ('AUDIT', 'audit'),
         ('BANNED', 'banned'),
     ]
-    status = models.CharField(max_length=10 , choices=STATUS_CHOICE,default='AUDIT')
+    status = models.CharField(max_length=10,  choices=STATUS_CHOICE, default='AUDIT')
     likes = models.BigIntegerField(default=0)
     page_views = models.BigIntegerField(default=0)
     create_time = models.DateTimeField()
@@ -34,4 +34,36 @@ class Post(models.Model):
         ('NORMAL', 'normal'),
         ('DELETE', 'delete'),
     ]
-    row_status = models.CharField(max_length=10,choices=STATUS_CHOICE,default='NORMAL')
+    row_status = models.CharField(max_length=10, choices=STATUS_CHOICE, default='NORMAL')
+
+class User(models.Model):
+    uuid = models.CharField(max_length=36)
+    username = models.CharField(max_length=32)
+    nickname = models.CharField(max_length=32)
+    password = models.CharField(max_length=32)
+    salt = models.CharField(max_length=16)
+    email = models.CharField(max_length=32)
+    STATUS_CHOICE = [
+        ('ADMIN', 'Admin'),
+        ('COMMON', 'Common'),
+    ]
+    user_type = models.CharField(max_length=10, choices=STATUS_CHOICE, default='COMMON')
+    STATUS_CHOICE = [
+        ('COMMON', 'Common'),
+        ('BANNED', 'Banned'),
+        ('INACTIVATION', 'Inactivation'),
+    ]
+    user_satus = models.CharField(max_length=10, choices=STATUS_CHOICE, default='INACTIVATION')
+    activation_code = models.CharField(max_length=32)
+    header_url = models.CharField(max_length=256)
+    create_time = models.DateTimeField()
+    STATUS_CHOICE = [
+        ('MALE', 'Male'),
+        ('FEMALE', 'Female'),
+        ('UNKNOWN', 'Unknown')
+    ]
+    gender = models.CharField(max_length=10, choices=STATUS_CHOICE, default='UNKNOWN')
+    brief = models.TextField()
+    likes = models.BigIntegerField(default=0)
+    page_views = models.BigIntegerField(default=0)
+
